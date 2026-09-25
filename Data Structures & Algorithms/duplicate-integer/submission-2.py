7b08da7c13ace2d08bfff659aa4c9a3f97e585b6
Ruth Olumo <155124566+olumoruth3@users.noreply.github.com>
2026-09-25 16:17:56 +0300

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        # Brute Force Soution checking every element agains every other element in the array. Has a time complexity of O(n^2) hence not so efficient
+        # for i in range(len(nums) - 1):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[j] == nums[i]:
+        #             return True
+        
+        # return False
+
+        # mySet = set()
+        # for num in nums:
+        #     if num in mySet:
+        #         return True
+        #     mySet.add(num)
+        # return False
+
+        mySet = set(nums)
+        if len(mySet) != len(nums):
+            return True
+        return False
